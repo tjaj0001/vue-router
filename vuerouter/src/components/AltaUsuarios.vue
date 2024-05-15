cec59b4cd3e53a0bf74ed2726e667be37fe7a1fc
Fernando Salas Carrion <fsc00016@red.ujaen.es>
2024-05-15 11:58:32 +0200

    commit modificacion Router.js y Vista Alta

--- vuerouter/src/components/AltaUsuarios.vue
+++ vuerouter/src/components/AltaUsuarios.vue
@@ -24,6 +24,10 @@
     Username:
     <input type="text" v-model="username" maxlength="20" />
   </label>
+  <div id="main" style="height: 50px; background-color: lightgray; width:200px;">
+    <!-- Contenido de prueba -->
+    <p>punto de referencia con id main para scroll Behavior.</p>
+  </div>
 </template>
 
 <style scoped>
--- vuerouter/src/router/router.js
+++ vuerouter/src/router/router.js
@@ -4,6 +4,7 @@ import Inicio from "@/views/VistaInicio.vue"    //manera 1 de importar el compon
 
 const routes = [
     {path: '/', name: 'inicio', component: Inicio },   //manera 1 de importar el componente
+    /*APLICACIÓN DE LAZY LOADING MEDIANTE LAS FUNCIONES IMPORT PARA REPARTIR LA CARGA DE COMPONENTES DE LA APLICACIÓN*/
     {path: '/alta', name: 'alta', component: () => import('@/views/VistaAlta.vue') },  //manera 2 de importarlo
     {path: '/listado/', name: 'listado', component: () => import('@/views/VistaListado.vue')},
     {path: '/borrado/:id', name: 'borrado', component: () => import('@/views/VistaBorrado.vue')},
@@ -14,5 +15,15 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),    //la url parecerá https://mipagina.com/inicio
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        // Siempre desplaza el scroll 10px por encima del elemento #main
+        // Si existe una posición guardada, vuelve a esa posición
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            // De lo contrario, se desplaza al selector '#main' con un desplazamiento de 10px por encima de este.
+            return { el: '#main', top: 10 }
+        }
+    },
 })
 export default router
